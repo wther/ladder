fb34d6dce05ef9ba75da2327ec33e61425f9d3f2
balazon <balazon@gmail.com>
2014-11-13 15:30:50 +0100

    a work in progress:
    token pictures done,
    figuring out how animation will work:
    the token is an image/sprite, currently testing kineticjs tweening animation between states

--- src/main/webapp/js/game.js
+++ src/main/webapp/js/game.js
@@ -132,6 +132,116 @@ function drawBoard(board) {
             }));
         }
         stage.add(ladderLayer);
+        
+        
+        var testLayer = new Kinetic.Layer();
+        
+        var spriteImage = new Image();
+        
+        var spriteLayer = new Kinetic.Layer();
+        spriteImage.onload = function() {
+    	  var sprite = new Kinetic.Sprite({
+    	    x: 10,
+    	    y: 550,
+    	    width: 50,
+    	    width: 50,
+    	    image: spriteImage,
+    	    animation: 'standing',
+    	    animations: {
+    	      standing: [
+    	        0, 0, 50, 50  	     
+    	      ],
+    	      kicking: [
+    	        // x, y, width, height (6 frames)
+    	        0, 109, 45, 98,
+    	        45, 109, 45, 98,
+    	        95, 109, 63, 98,
+    	        156, 109, 70, 98,
+    	        229, 109, 60, 98,
+    	        287, 109, 41, 98
+    	      ],
+    	      jumping: [
+            	0, 5, 50, 50,
+            	0, 10, 50, 50,
+            	0, 15, 50, 50
+    	      ]
+    	    },
+    	    frameRate: 7,
+    	    frameIndex: 0
+    	  });
+    	  spriteLayer.add(sprite);
+    	  stage.add(spriteLayer);
+    	  sprite.start();
+    	  
+    	  var currentX = 10;
+    	  var count = 9;
+    	  var myfunc = function () {
+    		  	if(count == 0) return;
+    		  	count--;
+			    currentX += 60;
+			    var tween = new Kinetic.Tween({
+		    		  node: sprite,
+		    		  x: currentX,
+		    		  y: 550,
+		    		  duration: 0.5,
+		    		  easing: Kinetic.Easings.BounceEaseOut
+			    });
+			    tween.onFinish =  myfunc;
+			    tween.play();
+    	  }
+    	  
+    	  myfunc();
+    	};
+    	spriteImage.src = '../images/token_blue.png';
+    	
+    	
+    	
+    	var imageObj2 = new Image();
+    	
+    	imageObj2.onload = function() {
+    	  var tokenImg = new Kinetic.Image({
+    	    x: 100,
+    	    y: 100,
+    	    image: imageObj2,
+    	    width: 100,
+    	    height: 100
+    	  });
+    	  testLayer.add(tokenImg);
+    	  stage.add(testLayer);
+    	};
+    	imageObj2.src = '../images/token_red.png';
+    	
+
+        var imageObj = new Image();
+        imageObj.onload = function() {
+          var yoda = new Kinetic.Image({
+            x: 200,
+            y: 50,
+            image: imageObj,
+            width: 106,
+            height: 118
+          });
+
+          // add the shape to the layer
+          testLayer.add(yoda);
+
+          // add the layer to the stage
+          stage.add(testLayer);
+        };
+        imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/yoda.jpg';
+    	
+//    	var mySprite = new Kinetic.Sprite({
+//    		x: 100,
+//     	    y: 100,
+//     	    width: 500,
+//     	    height: 500,
+//     	    image: spriteImage,
+//        });
+//    	mySprite.fill("red");
+//    	testLayer.add(mySprite);
+    	
+    	
+        
 }
 
 /**
